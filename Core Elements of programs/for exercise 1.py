d2808271e6467fb762e3b82d0ed520d2e30177cf
shadow81627 <shadow81627@gmail.com>
2016-09-05 01:01:10 +1000

    Added for loop exercises

---
+++ Core Elements of programs/for exercise 1.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  5 00:20:51 2016
+
+@author: Robinson
+"""
+for i in range(2, 11, 2):
+    print(i)
+print("Goodbye!")
---
+++ Core Elements of programs/for exercise 2.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  5 00:20:51 2016
+
+@author: Robinson
+"""
+
+print("Hello!")
+for i in range(10, 0, -2):
+    print(i)
