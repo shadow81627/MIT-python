3bebd5cc4fa427706ab627c03faeb02ac1883ddc
shadow81627 <shadow81627@gmail.com>
2016-09-05 17:41:23 +1000

    Finished problem 3

--- Problem Set 1/Problem 3.py
+++ Problem Set 1/Problem 3.py
@@ -5,10 +5,20 @@ Created on Mon Sep  5 15:11:27 2016
 @author: Robinson
 """
 
-s = 'azcbobobegghakl'
+s = 'zyxwvutsrqponmlkjihgfedcba'
 letters = ""
-search = "abcdefghijklmnopqrstuvwxyz"
+print(letters)
+lastLetter = "a"
+longest = ""
 for letter in s:
-    
-    if chr(letter) <
-print("Longest substring in alphabetical order is: " + str(numBob))
+    if ord(letter) >= ord(lastLetter): 
+        letters += letter
+        lastLetter = letter
+        if len(letters) > len(longest):
+            longest = letters
+    else:
+        lastLetter = letter
+        if len(letters) > len(longest):
+            longest = letters
+        letters = letter
+print("Longest substring in alphabetical order is: " + longest)
