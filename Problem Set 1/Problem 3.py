5f918dde028e486a3acbd548639085da7ad5c08e
shadow81627 <shadow81627@gmail.com>
2016-09-05 15:51:27 +1000

    Added Problem 3

---
+++ Problem Set 1/Problem 3.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  5 15:11:27 2016
+
+@author: Robinson
+"""
+
+s = 'azcbobobegghakl'
+letters = ""
+search = "abcdefghijklmnopqrstuvwxyz"
+for letter in s:
+    
+    if chr(letter) <
+print("Longest substring in alphabetical order is: " + str(numBob))
