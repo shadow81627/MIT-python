17eaa973613d024d67919e8a09aec4b26680a4c4
shadow81627 <shadow81627@gmail.com>
2016-09-09 17:06:51 +1000

    Added boiler plate code

--- week 2/guess my number.py
+++ week 2/guess my number.py
@@ -5,5 +5,20 @@ Created on Fri Sep  9 16:22:28 2016
 @author: Robinson
 """
 
-number = input("Please think of a number between 0 and 100!")
-print(str(number))
+print("Please think of a number between 0 and 100!")
+#initialise the varible for the input to be stored in
+input = " "
+
+#bottom end of the range to guess between
+low = 0
+
+#top end of the range to guess between
+high = 100
+
+#initialise of the number the program will guess as the middle point of the range
+number =  int((high - low)/2)
+
+#
+while input != "c":
+    print("Is your secret number " + str(number) + "?")
+    
