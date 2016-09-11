0190a601386111808e086bf59b0b286fcb1dbc62
shadow81627 <shadow81627@gmail.com>
2016-09-11 23:43:10 +1000

    Added Odd
    Created a function that will return True if the given number is odd and
    False if the number is even.

---
+++ week 2/odd.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 11 23:32:54 2016
+
+@author: Damien Robinson
+"""
+
+def odd(x):
+    '''
+    x: int or float.
+
+    returns: True if x is odd, False otherwise
+    '''
+    return x % 2 != 0 
