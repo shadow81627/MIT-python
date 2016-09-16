32181263eda7ce50a6979697059c7bac4e489a8c
shadow81627 <shadow81627@gmail.com>
2016-09-16 18:35:13 +1000

    Refactored Problem 1
    Added for loop to iterate through the monthly repayments.

--- week 2/Porblem Set 2/Problem 1 - Paying Debt off in a Year.py
+++ week 2/Porblem Set 2/Problem 1 - Paying Debt off in a Year.py
@@ -1,17 +1,36 @@
 # -*- coding: utf-8 -*-
 """
+Calculates the credit card balance after one year if a person only pays the 
+minimum monthly payment required by the credit card company each month.
+
 Created on Wed Sep 14 23:23:59 2016
 
 @author: Damien Robinson
 """
+#The outstanding balance on the credit card
+balance = 484
 
-balance = 42
+#Annual interest rate as a decimal
 annualInterestRate = 0.2
+
+#Minimum monthly payment rate as a decimal
 monthlyPaymentRate = 0.04
 
-Remaining balance:
+#Duration of the repayment period in years
+duration = 1
+
+for i in range(0, duration * 12):
+    #Calculate the Monthly interest rate
+    monthlyInterestRate = annualInterestRate / 12.0
     
-Monthly interest rate= (Annual interest rate) / 12.0
-Minimum monthly payment = (Minimum monthly payment rate) x (Previous balance)
-Monthly unpaid balance = (Previous balance) - (Minimum monthly payment)
-Updated balance each month = (Monthly unpaid balance) + (Monthly interest rate x Monthly unpaid balance) 
+    #minimum monthly payment is calculated as monthly payment rate  based on balance
+    payment = monthlyPaymentRate * balance
+    
+    #monthly Unpaied Balance is prvious balance  - monthly payment
+    unpaidBalance = balance - payment
+    
+    #New monthly balance with interest
+    balance = unpaidBalance + (monthlyInterestRate * unpaidBalance)
+    
+#print the remianing balance
+print("Remaining balance: " + str(round(balance, 2)))
