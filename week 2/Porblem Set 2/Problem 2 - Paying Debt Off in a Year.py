31c51fbbf2f945d8eb5a8fa60e8b166f3a2468a5
shadow81627 <shadow81627@gmail.com>
2016-09-16 18:42:01 +1000

    Added boiler plate code
    Added place holder code for problem 2

--- week 2/Porblem Set 2/Problem 2 - Paying Debt Off in a Year.py
+++ week 2/Porblem Set 2/Problem 2 - Paying Debt Off in a Year.py
@@ -5,3 +5,10 @@ Created on Fri Sep 16 18:40:02 2016
 @author: Damien Robinson
 """
 
+    balance - the outstanding balance on the credit card
+
+    annualInterestRate - annual interest rate as a decimal
+
+Monthly interest rate = (Annual interest rate) / 12.0
+Monthly unpaid balance = (Previous balance) - (Minimum fixed monthly payment)
+Updated balance each month = (Monthly unpaid balance) + (Monthly interest rate x Monthly unpaid balance) 
