4242afeb021a3ed99bf0b981b8a5cccf69a75a8c
shadow81627 <shadow81627@gmail.com>
2016-09-16 20:42:12 +1000

    Solved Problem 2

--- week 2/Porblem Set 2/Problem 2 - Paying Debt Off in a Year.py
+++ week 2/Porblem Set 2/Problem 2 - Paying Debt Off in a Year.py
@@ -5,10 +5,32 @@ Created on Fri Sep 16 18:40:02 2016
 @author: Damien Robinson
 """
 
-    balance - the outstanding balance on the credit card
+#balance - the outstanding balance on the credit card
+balance =  36
+	     
+#annualInterestRate - annual interest rate as a decimal
+annualInterestRate = 0.2
 
-    annualInterestRate - annual interest rate as a decimal
+#payment to be made each month
+payment = 0
 
-Monthly interest rate = (Annual interest rate) / 12.0
-Monthly unpaid balance = (Previous balance) - (Minimum fixed monthly payment)
-Updated balance each month = (Monthly unpaid balance) + (Monthly interest rate x Monthly unpaid balance) 
+#monthly balance with interest
+updatedBalanceEachMonth = balance
+
+#while there is still a balance keep trying higher repayments
+while updatedBalanceEachMonth > 0:
+    #reset updatedBalanceEachMonth 
+    updatedBalanceEachMonth = balance
+    #increment payment by 10
+    payment += 10
+    
+    #make repayments for 12 months
+    for i in range(0, 12):
+        #Calculate the Monthly interest rate
+        monthlyInterestRate = annualInterestRate / 12.0
+        #monthly Unpaied Balance is prvious balance  - monthly payment
+        unpaidBalance = updatedBalanceEachMonth - payment
+        #New monthly balance with interest
+        updatedBalanceEachMonth = unpaidBalance + monthlyInterestRate * unpaidBalance
+            
+print("Lowest Payment: " + str(payment))
