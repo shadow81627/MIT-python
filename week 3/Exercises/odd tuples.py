07d2e58d524cc612044a9c5ed048fec5961c9cc4
shadow81627 <shadow81627@gmail.com>
2016-09-16 23:09:26 +1000

    Added odd tuples

---
+++ week 3/Exercises/odd tuples.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 16 23:08:52 2016
+
+@author: Robinson
+"""
+
+def oddTuples(aTup):
+    '''
+    aTup: a tuple
+    
+    returns: tuple, every other element of aTup. 
+    '''
+    # Your Code Here
