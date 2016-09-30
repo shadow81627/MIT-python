71c71afec1058cc128d17a42ab355ebdef8de358
shadow81627 <shadow81627@gmail.com>
2016-09-30 23:21:10 +1000

    Added biggest function
    '''
    aDict: A dictionary, where all the values are lists.
    
    returns: The key with the largest number of values associated with it
    '''

--- week 3/Exercises/biggest.py
+++ week 3/Exercises/biggest.py
@@ -5,9 +5,15 @@ Created on Fri Sep 30 22:37:49 2016
 @author: Damien Robinson
 """
 
-def how_many(aDict):
+def biggest(aDict):
     '''
     aDict: A dictionary, where all the values are lists.
 
     returns: The key with the largest number of values associated with it
     '''
+    result = {'lenght': 0}
+    for key in aDict.keys():
+        if len(aDict[key]) > result['lenght']:
+            result['key'] = key
+            result['lenght'] = len(aDict[key])
+    return result['key']
