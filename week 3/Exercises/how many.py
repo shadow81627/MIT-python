2ec018e86661409df3fb4614e576e7f9f7c61ea9
shadow81627 <shadow81627@gmail.com>
2016-09-30 22:59:11 +1000

    Added how_many function
    aDict: A dictionary, where all the values are lists.
    
    returns: int, how many values are in the dictionary.

--- week 3/Exercises/how many.py
+++ week 3/Exercises/how many.py
@@ -5,3 +5,13 @@ Created on Fri Sep 30 22:37:49 2016
 @author: Damien Robinson
 """
 
+def how_many(aDict):
+    '''
+    aDict: A dictionary, where all the values are lists.
+
+    returns: int, how many values are in the dictionary.
+    '''
+    num = 0
+    for list in aDict:
+        num += len(list)
+    return num
