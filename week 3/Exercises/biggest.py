7c12819732a5896deeacd0b95f18101ac8e6c3ea
shadow81627 <shadow81627@gmail.com>
2016-09-30 23:19:39 +1000

    Added biggest

---
+++ week 3/Exercises/biggest.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 30 22:37:49 2016
+
+@author: Damien Robinson
+"""
+
+def how_many(aDict):
+    '''
+    aDict: A dictionary, where all the values are lists.
+
+    returns: The key with the largest number of values associated with it
+    '''
