41a96f68fa8fe51a804bdca9ffdfa329b4222a76
shadow81627 <shadow81627@gmail.com>
2016-10-02 00:48:40 +1000

    Implemented isWordGuessed function

--- week 3/Problem Set 3/Problem 1/ps3_hangman.py
+++ week 3/Problem Set 3/Problem 1/ps3_hangman.py
@@ -50,7 +50,10 @@ def isWordGuessed(secretWord, lettersGuessed):
     returns: boolean, True if all the letters of secretWord are in lettersGuessed;
       False otherwise
     '''
-    # FILL IN YOUR CODE HERE...
+    for letter in secretWord:
+        if letter not in lettersGuessed:
+            return False
+    return True
 
 
 
