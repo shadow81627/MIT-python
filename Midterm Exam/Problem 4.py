dc1787b404f951e0eeea0eab184b5644a794c99d
shadow81627 <shadow81627@gmail.com>
2016-10-04 23:50:47 +1000

    Added Problem 4

---
+++ Midterm Exam/Problem 4.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  4 23:45:16 2016
+
+@author: Damien Robinson
+"""
+
+def closest_power(base, num):
+    '''
+    base: base of the exponential, integer > 1
+    num: number you want to be closest to, integer > 0
+    Find the integer exponent such that base**exponent is closest to num.
+    Note that the base**exponent may be either greater or smaller than num.
+    In case of a tie, return the smaller value.
+    Returns the exponent.
+    '''
+    # Your code here
