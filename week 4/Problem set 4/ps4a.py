251ecd681558b138a42c513be6c96c953652cce5
shadow81627 <shadow81627@gmail.com>
2016-10-19 01:18:57 +1000

    Added playGame function
    Allow the user to play an arbitrary number of hands.
    
    1) Asks the user to input 'n' or 'r' or 'e'.
    * If the user inputs 'n', let the user play a new (random) hand.
    * If the user inputs 'r', let the user play the last hand again.
    * If the user inputs 'e', exit the game.
    * If the user inputs anything else, tell them their input was invalid.
    
    2) When done playing the hand, repeat from step 1

--- week 4/Problem set 4/ps4a.py
+++ week 4/Problem set 4/ps4a.py
@@ -285,9 +285,27 @@ def playGame(wordList):
  
     2) When done playing the hand, repeat from step 1    
     """
-    # TO DO ... <-- Remove this comment when you code this function
-    print("playGame not yet implemented.") # <-- Remove this line when you code the function
-   
+  
+    hand = {}
+    n = HAND_SIZE
+    while True:
+        userInput = input('Enter n to deal a new hand, r to replay the last hand, or e to end game: ')
+        if userInput == 'e':
+            #dont do anything
+            break
+            print()
+        elif userInput == 'n':
+            hand = dealHand(n).copy()
+            playHand(hand, wordList, n)
+        elif userInput == 'r':
+            if len(hand) > 0:
+                playHand(hand, wordList, n)
+            else:
+                print('You have not played a hand yet. Please play a new hand first!')
+        else:
+            print('Invalid command.')
+
+       
 
 
 
